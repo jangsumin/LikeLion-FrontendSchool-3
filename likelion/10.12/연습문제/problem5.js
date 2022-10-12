f80871e44ce23ed8988132332b2be5b205d7aef0
jangsumin <jsm0726@naver.com>
2022-10-12 22:20:51 +0900

    modify source code in problem5, problem6 after code review

--- likelion/10.12/연습문제/problem5.js
+++ likelion/10.12/연습문제/problem5.js
@@ -1,3 +1,3 @@
-// 로또 만들기
-// console.log(Math.floor(Math.random()*10));
-console.log([0, 1, 2, 3, 4, 5].map(el => Math.floor(Math.random()*10)));
+// 로또 만들기, Math.random은 0이상 1미만의 값이 추출된다!
+// console.log(Math.floor(Math.random()*45));
+console.log([0, 1, 2, 3, 4, 5].map(el => Math.floor(Math.random()*45) + 1));
--- likelion/10.12/연습문제/problem6.js
+++ likelion/10.12/연습문제/problem6.js
@@ -1,19 +1,26 @@
 // 숫자에 단위 콤마 표시하고, 삭제하기.
 let won = 100000000;
-let temp = '';
+let temp = [];
+let str = '';
 
 // 표시하기
-const arr = String(won).split("");
-for (const i in arr) {
-    if(i % 3 === 0 && i / 3 >= 1) {
-        temp += ',';
+const arr = String(won).split("").reverse();
+for (let i=0; i < arr.length; i++) {
+    if(i % 3 === 0 && i !== 0) {
+        temp.push(',');
     }
-    temp += arr[i];
+    temp.push(arr[i]);
 }
 
-console.log(temp);
+str = temp.reverse().join("");
+
+console.log(str);
 
 // 제거하기
+
+// let 숫자 = prompt("숫자를 콤마로 구분하여 입력해주세요.").split("");
+// console.log(숫자.filter((a) => a !== ",").join(""));
+
 let temp2 = '';
 function deleteComma(temp) {
     const arr2 = temp.split("");
@@ -29,4 +36,4 @@ function deleteComma(temp) {
     return parseInt(temp2, 10);
 }
 
-console.log(deleteComma(temp));
+console.log(deleteComma(str));
